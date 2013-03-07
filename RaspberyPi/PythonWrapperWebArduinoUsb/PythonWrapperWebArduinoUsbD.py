a1fb20838d42951f6a055e34949b901f2db9f0ff
charles <charles@Mercure>
2013-03-07 07:54:18 +0100

    Python deamon-server to interact with USB and socket in same time using Select python module

--- RaspberyPi/PythonWrapperWebArduinoUsb/PythonWrapperWebArduinoUsbD.py
+++ RaspberyPi/PythonWrapperWebArduinoUsb/PythonWrapperWebArduinoUsbD.py
@@ -4,6 +4,8 @@ import serial, time
 import sys
 import socket
 import datetime
+import select
+import sys
 import sqlite3
 from optparse import OptionParser
 
@@ -11,51 +13,64 @@ parser = OptionParser(usage="usage: %prog [options] filename",version="%prog 1.0
 parser.add_option("-s", "--send",action="store",dest="aMsgToSend",default="20",help="Command to send to Arduino Leonardo")
 (options, args) = parser.parse_args()
 
-HOST = ''                 # Symbolic name meaning all available interfaces
-PORT = 50007              # Arbitrary non-privileged port
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(1)
-#s.setblocking(0)
+fd = serial.Serial('/dev/ttyACM0', 9600, timeout=5)
 
-
-
+host = ''
+port = 50007
+backlog = 5
+size = 1024
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host,port))
+server.listen(backlog)
+input = [server,sys.stdin,fd]
 while 1:
-    print ("loop")
-    conn, addr = s.accept()
-    print 'Connected by', addr
-    data = conn.recv(1024)
-    if data:
-        print ("data : " + str(data))
-        aCurrentDateTime = datetime.datetime.now()
-        #aLogLine = "DATE: " + str(aCurrentDateTime) + " ORIGIN: " + options.aOriginator + " CMD: " + options.aMsgToSend + " ID: " + options.aRequestorId
-        #print ("Log line : " + aLogLine)
-        #aLogFile = open("/var/www/Logs/logs.txt", "a")
-        #aLogFile.write(aLogLine+"\n")
-        #aLogFile.close()
+    inputready,outputready,exceptready = select.select(input,[],[])
 
-        #print ("Open virtual serial port")
-        #fd = serial.Serial('/dev/ttyACM0', 9600, timeout=5)
-        #fd.flush()
-        #print ("Sending : " + options.aMsgToSend)
+    for s in inputready:
 
-        #fd.write(options.aMsgToSend)
+        if s == server:
+            print ("handle the server socket")
+            client, address = server.accept()
+            input.append(client)
 
-        #sqliteCnx = sqlite3.connect('/var/www/DataBase/Domos.db')
+        elif s == sys.stdin:
+            print ("handle standard input")
+            aInput = sys.stdin.readline()
+            print ("input is : " + aInput)
+            fd.write(aInput)
+            
+        elif s == fd:
+            print ("handle USB port input")
+            aResponse = fd.readline()
+            print ("USB response : " + aResponse)
+            sqliteCnx = sqlite3.connect('/var/www/DataBase/Domos.db')
+            c = sqliteCnx.cursor()
+            expires = datetime.datetime.now()
+            aValueReceived=aResponse
+            aRequestorId="UNKNOW"
+            if("ID" in aResponse):
+                aValueReceived = (aResponse.split('_')[1]).split(':')[1]
+                aRequestorId = (aResponse.split('_')[0]).split(':')[1]
+            c.execute("INSERT INTO measures (id, timestamp, value) VALUES (?,?,?)",(aRequestorId,expires,float(aValueReceived)))
+            sqliteCnx.commit()
+            sqliteCnx.close()
 
-    #aResponse = fd.readline() #How do I get the most recent line sent from the device?
-    if(aResponse != ""):
-        #c = sqliteCnx.cursor()
-        print ("Response : " + aResponse)
-        #expires = aCurrentDateTime
-        #if("ID" in aResponse):
-            #aValueReceived = (aResponse.split('_')[1]).split(':')[1]
-            #c.execute("INSERT INTO measures (id, timestamp, value) VALUES (?,?,?)",(options.aRequestorId,expires,float(aValueReceived)))
-            #sqliteCnx.commit()
-            #print c.fetchone()
-            #sqliteCnx.close()
-            #sys.exit(0)
-
-conn.close()
-
-
+        else:
+            print ("handle all other sockets")
+            data = s.recv(size)
+            print ("input is : " + data)
+            if data:
+                print ("Writting input to USB port and sending back to sender")
+                aCurrentDateTime = datetime.datetime.now()
+                aLogLine = "DATE: " + str(aCurrentDateTime) + " ORIGIN: "  + " CMD: " + data + " ID: " 
+                print ("Log line : " + aLogLine)
+                aLogFile = open("/var/www/Logs/logs.txt", "a")
+                aLogFile.write(aLogLine+"\n")
+                aLogFile.close()
+                fd.write(data)
+                s.send(data)
+            else:
+                print ("Closing socket")
+                s.close()
+                input.remove(s)
+server.close() 
--- RaspberyPi/PythonWrapperWebArduinoUsb/PythonWrapperWebArduinoUsbS.py
+++ RaspberyPi/PythonWrapperWebArduinoUsb/PythonWrapperWebArduinoUsbS.py
@@ -14,7 +14,7 @@ PORT = 50007          # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 s.connect((HOST, PORT))
-s.send('Hello, world')
+s.send('J')
 
 data = s.recv(1024)
 
