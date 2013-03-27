7154c828e694a674c8ec53e1c11d7a0889df5f1c
charles <charles@Mercure>
2013-03-27 21:28:11 +0100

    Clean up and do a smater process

--- ArduinoUnoEntree/ArduinoUnoEntree.ino
+++ ArduinoUnoEntree/ArduinoUnoEntree.ino
@@ -11,10 +11,7 @@
 //Pin definition
 const int _InPinIrDetector = 7;
 const int _OutPinRelay = 11;
-const int _InPinLedMeasure = A0;
-const int _InPinMoistureMeasure = A1;
 const int _InPinDht22 = 10;
-const int _OutPinBuz1 = 9;
 
 //Xbee objects
 //create Xbee object to control a Xbee
@@ -23,7 +20,6 @@ XBee _Xbee = XBee();
 ZBRxResponse _ZbRxResp = ZBRxResponse(); 
 //Global variable used in the program
 int _CmdReceived = 0;
-int _AllowAutoLight = 1;
 int _DataToSend = 0;
 bool _TimerExpire = true;
 DHT22 _Dht22(_InPinDht22); //Setup a DHT22 instance
@@ -48,8 +44,10 @@ void setup()
   digitalWrite(_OutPinRelay, LOW);
   
   //Set timer to 10seconds
-  MsTimer2::set(60000, InterruptTimer2);
-  delay(1000);
+  MsTimer2::set(120000, InterruptTimer2);
+  
+  //Wait 5 minutes before starting anything
+  delay(5000);
 }
 
 void loop()
@@ -59,22 +57,6 @@ void loop()
   _Xbee.readPacket();
   //Read button status
   int aInputDigitalValue = digitalRead(_InPinIrDetector);
-
-  if (aInputDigitalValue == HIGH)
-  {
-    if (_AllowAutoLight == 1)
-    {
-      _TimerExpire = false;
-      digitalWrite(_OutPinRelay, HIGH);
-      MsTimer2::start(); // active Timer 2 
-    }
-    if (_TimerExpire == true)
-    {
-        //2/Send the detection info to central
-        unsigned int aCommand = 3;
-        //sendZigBeeMsg(aCommand, COORD_ADDR);
-    }
-  }
 
   if (_Xbee.getResponse().isAvailable()) 
   {
@@ -86,17 +68,27 @@ void loop()
       _CmdReceived = _ZbRxResp.getData(0);
     }  
   }
+  
+  if (aInputDigitalValue == HIGH)
+  {
+    digitalWrite(_OutPinRelay, HIGH);
+    MsTimer2::start(); // active Timer 2 
+    if (_TimerExpire == true)
+    {
+        _TimerExpire = false;
+        _CmdReceived = 50;
+        _DataToSend=440;
+    }
+  }
 
   //Do the real action
   if(_CmdReceived==34)
   {
     //This command allow the master to turn the light on
-    //flashPin(_OutPinRelay, _CmdReceived, 400);
     digitalWrite(_OutPinRelay, HIGH);
   }
   else if(_CmdReceived==35)
   {
-    
     //This command allow the master to turn the light off
     digitalWrite(_OutPinRelay, LOW);
   }
@@ -106,27 +98,12 @@ void loop()
     digitalWrite(_OutPinRelay, HIGH);
     MsTimer2::start(); // active Timer 2 
   }
-  else if(_CmdReceived==37)
-  {
-     _AllowAutoLight=1;
-  }
-  else if(_CmdReceived==38)
-  {
-     _AllowAutoLight=0;
-  }
   else if(_CmdReceived==33)
   {
   //for debuging; simulate light on event
-     //2/Send the detection info to central
 	 delay(1000);
-    unsigned int aCommand = 3;
-    sendZigBeeMsg(aCommand, COORD_ADDR);
-  }
-  else if(_CmdReceived==32)
-  {
-     _DataToSend=analogRead(_InPinLedMeasure);
-    Serial.print("Light Value : ");
-    Serial.println(_DataToSend);
+    _CmdReceived = 50;
+    _DataToSend=445;
   }
   else if((_CmdReceived==30)||(_CmdReceived==31))
   {
@@ -147,21 +124,17 @@ void loop()
       Serial.print("/");
       Serial.print(_Dht22.getHumidityAsInt());
       Serial.println("%");
-      if ((_CmdReceived==2)||(_CmdReceived==30))
+      if (_CmdReceived==30)
       {
         _DataToSend=_Dht22.getTemperatureCAsInt();
       }
-      else if((_CmdReceived==3)||(_CmdReceived==31))
+      else if(_CmdReceived==31)
       {
         _DataToSend=_Dht22.getHumidityAsInt();
       }
       break;
     case DHT_ERROR_CHECKSUM:
       Serial.print("check sum error ");
-      Serial.print(_Dht22.getTemperatureCAsFloat());
-      Serial.print("C ");
-      Serial.print(_Dht22.getHumidityAsFloat());
-      Serial.println("%");
       break;
     case DHT_BUS_HUNG:
       Serial.println("BUS Hung ");
@@ -187,6 +160,6 @@ void loop()
     //Send the response if necessary
   if(_DataToSend!=0)
   {
-    sendZigBeeMsg2(_CmdReceived,_DataToSend,COORD_ADDR);
+    sendZigBeeMsg2(_Xbee,_CmdReceived,_DataToSend,COORD_ADDR);
   }
 }
--- RaspberyPi/PythonWrapperWebArduinoUsb/PythonWrapperWebArduinoUsbD.py
+++ RaspberyPi/PythonWrapperWebArduinoUsb/PythonWrapperWebArduinoUsbD.py
@@ -7,37 +7,44 @@ import socket
 import datetime
 import select
 import sys
-
 import json
-
 # Import smtplib to provide email functions
 import smtplib
 # Import the email modules
 from email.mime.text import MIMEText
-
 import sqlite3
 from optparse import OptionParser
+import logging
 
 class Object:
-    "Encore une nouvelle classe temporelle"
+    "Une classe qui decrit un capteur"
+    
     def __init__(self):
         self.id =0
         self.physicalLocation =""
         self.currentStatus =""
+        self.description =""
+        self.Reset =""
         self.InPossibleCmd ={}
         self.OutPossibleCmd ={}
         self.ActionsCommands ={}
         self.PossibleStates ={}
         
     def executeCmd(self,aCmdFromData):
-        print ("executing : " + str(aCmdFromData) )
+        #logging.info ("executing : " + str(aCmdFromData) )
         exec(self.ActionsCommands[aCmdFromData])
+        
+    def reset(self):
+        logging.debug("reseting")
+        exec(self.Reset)
 
     def __repr__(self):
         aRetString = ""
         aRetString = aRetString + "self.id : " + str(self.id) + "\n"
         aRetString = aRetString + "self.physicalLocation : " + str(self.physicalLocation) + "\n"
         aRetString = aRetString + "self.currentStatus : " + str(self.currentStatus) + "\n"
+        aRetString = aRetString + "self.description : " + str(self.description) + "\n"
+        aRetString = aRetString + "self.Reset : " + str(self.Reset) + "\n"
         aRetString = aRetString + "self.InPossibleCmd : " + str(self.InPossibleCmd) + "\n"
         aRetString = aRetString + "self.OutPossibleCmd : " + str(self.OutPossibleCmd) + "\n"
         aRetString = aRetString + "self.ActionsCommands : " + str(self.ActionsCommands) + "\n"
@@ -46,7 +53,7 @@ class Object:
 
 
 def sendEmailFireDetected():
-    print("Envoi email detection incendie")
+    logging.info("Envoi email detection incendie")
     # Define email addresses to use
     addr_to   = Config["addr_to"]
     addr_from = Config["addr_from"]
@@ -68,107 +75,162 @@ def sendEmailFireDetected():
     s.sendmail(addr_from, addr_to, msg.as_string())
     s.quit()
 
-def PeopleDetectedCharlesRoom(iSerialLink):
-    print("Detection chambre charles")
+def PeopleDetectedCharlesRoom(iSerialLink,iListOfDevice):
+    logging.info("Detection chambre charles")
     aDataToWrite = "MSG:11_ORIGIN:PythonScript"
-    SendMessage(iSerialLink, aDataToWrite)
+    SendMessage(iSerialLink, aDataToWrite,iListOfDevice)
     
 def PeopleDetectedEntree(iSerialLink):
-    print("Detection entree")
+    logging.info("Detection entree")
     #aDataToWrite = "MSG:11_ORIGIN:PythonScript"
     #SendMessage(iSerialLink, aDataToWrite)
     
-def SendMessage(iSerialLink,iDataToWrite):
-    print ("Writting input to USB port and sending back to sender")
+def smartProcessing(iListOfDevice,iSerialLink):
+    logging.info("Trying to be smart")
+    for aOneDevice in iListOfDevice:
+        logging.debug("checking : " + str(aOneDevice))
+        if ((aOneDevice.id == 2) and (aOneDevice.currentStatus=="incendie en cours")):
+            sendEmailFireDetected()
+        elif ((aOneDevice.id == 6) and (aOneDevice.currentStatus=="personne detecte")):
+            PeopleDetectedEntree()
+        elif ((aOneDevice.id == 7) and (aOneDevice.currentStatus=="personne detecte")):
+            PeopleDetectedCharlesRoom(iSerialLink,iListOfDevice)
+        else :
+            logging.debug("Nothing to do even if we are smart")
+        aOneDevice.reset()
+    
+    
+def SendMessage(iSerialLink,iDataToWrite, iListOfDevice):
+    logging.info ("Writting input to USB port and sending back to sender")
     aCurrentDateTime = datetime.datetime.now()
     aCmdFromData=int((iDataToWrite.split('_')[0]).split(':')[1])
     aOriginFromData=(iDataToWrite.split('_')[1]).split(':')[1]
-    #print ("chr(0x1F):" + chr(0x1F) + "_chr(31):" + chr(aCmdFromData))
     aLogLine = "(V2)TO:USB DATE: " + str(aCurrentDateTime) + " ORIGIN: " + aOriginFromData  + " CMD: " + str(aCmdFromData)
     
-    #sqliteCnx = sqlite3.connect('/var/www/DataBase/Domos.db')
-    #c = sqliteCnx.cursor()
     #c.execute("update object set status=off where id=?",(aCmdFromData))
-    #sqliteCnx.commit()
-    #sqliteCnx.close()
     
-    print ("Log line : " + aLogLine)
+    logging.info (aLogLine)
     aLogFile = open("/var/www/Logs/logs.txt", "a")
     aLogFile.write(aLogLine+"\n")
     aLogFile.close()
     
-    for aOneObj in aRegisterDevices:
-        print ("checking if command is for : " + str(aOneObj) )
-        print ("possible cmd : " + str(aOneObj.InPossibleCmd.keys()))
+    logging.info("Looping on the devices and allow them to update themselve.")
+    for aOneObj in iListOfDevice:
+        #logging.info ("possible cmd : " + str(aOneObj.InPossibleCmd.keys()))
         if aCmdFromData in aOneObj.InPossibleCmd.keys():
-            print ("It is ;)")
+            logging.info("Updating device ID : " + str(aOneObj.id))
             aOneObj.executeCmd(aCmdFromData)
-            #exec(aOneObj.ActionsCommands[aCmdFromData])
-            #self.currentStatus="on"
     
     iSerialLink.write(chr(aCmdFromData))
     
-def HandleUsbInput(iUsbString):
-    print ("USB response : " + iUsbString)
+def HandleUsbInput(iUsbString,iListOfDevice):
+    logging.info ("Handle USB incoming message : " + iUsbString)
     if("ID" in iUsbString):
         sqliteCnx = sqlite3.connect('/var/www/DataBase/Domos.db')
         c = sqliteCnx.cursor()
         expires = datetime.datetime.now()
         aValueReceived = float((iUsbString.split('_')[1]).split(':')[1])
-        aRequestorId = (iUsbString.split('_')[0]).split(':')[1]
-        c.execute("INSERT INTO measures (id, timestamp, value) VALUES (?,?,?)",(aRequestorId,expires,aValueReceived))
+        aRequestorId = int((iUsbString.split('_')[0]).split(':')[1])
+        c.execute("INSERT INTO measures (id, timestamp, value) VALUES (?,?,?)",(str(aRequestorId),expires,aValueReceived))
         sqliteCnx.commit()
         sqliteCnx.close()
         
         aCurrentDateTime = datetime.datetime.now()
-        aLogLine = "(V2)FROM:USB DATE: " + str(aCurrentDateTime) + " ORIGIN: " + aRequestorId  + " VALUE: " + str(aValueReceived)
-        print ("Log line : " + aLogLine)
+        aLogLine = "(V2)FROM:USB DATE: " + str(aCurrentDateTime) + " ORIGIN: " + str(aRequestorId)  + " VALUE: " + str(aValueReceived)
+        logging.info (aLogLine)
         aLogFile = open("/var/www/Logs/logs.txt", "a")
         aLogFile.write(aLogLine+"\n")
         aLogFile.close()
         
-        if(aRequestorId == "1"):
-            print("OMG...Hell on earth1")
-            sendEmailFireDetected()
-        elif (aRequestorId == "2"):
-            print("OMG...Hell on earth2")
-            PeopleDetectedCharlesRoom(fd)
-        elif (aRequestorId == "80"):
-            print("OMG...Hell on earth3")
-            PeopleDetectedEntree(fd)
-        else :
-            print ("KO")
+        logging.info("Looping on the devices and allow them to update themselve.")    
+        for aOneObj in iListOfDevice:
+            #logging.info ("possible cmd : " + str(aOneObj.OutPossibleCmd.keys()))
+            if aRequestorId in aOneObj.OutPossibleCmd.keys():
+                logging.info("Updating device ID : " + str(aOneObj.id))
+                aOneObj.executeCmd(aRequestorId)
     else:
-        print("Strange response....ignore it")
+        logging.error("Strange response....ignore it")
 
 parser = OptionParser(usage="usage: %prog [options]",version="%prog 1.0")
 parser.add_option("-p", "--port",action="store",dest="aPortToUse",default="50007",help="the port to listen")
 (options, args) = parser.parse_args()
 
+logging.basicConfig(filename='PythonWrapperWebArduinoUsbD.log',level=logging.INFO)
+logging.info('Daemon starting...')
+
 fd = serial.Serial('/dev/ttyACM0', 9600, timeout=5)
 
 json_data = open('ConfigFiles')
 Config = json.load(json_data)
-print ("config is : " + str(Config))
+logging.info("config is : " + str(Config))
 
 #Create static object (the modules bought since I can not change them to register themselve)
 aLampeCharles = Object()
 aLampeCharles.physicalLocation = "Charles"
+aLampeCharles.id = 1
+aLampeCharles.description = "Lampe charles"
+aLampeCharles.Reset = "self.currentStatus=\"on\""
 aLampeCharles.InPossibleCmd = {5:"on",6:"off"}
 aLampeCharles.OutPossibleCmd = {}
 aLampeCharles.PossibleStates = ["on","off"]
 aLampeCharles.ActionsCommands ={5:"self.currentStatus=\"on\"", 6:"self.currentStatus=\"off\""}
 
+aVoletCharles = Object()
+aVoletCharles.physicalLocation = "Charles"
+aVoletCharles.id = 4
+aVoletCharles.description = "Volet charles"
+aVoletCharles.InPossibleCmd = {7:"ouvert",8:"ferme"}
+aVoletCharles.OutPossibleCmd = {}
+aVoletCharles.PossibleStates = ["ouvert","ferme"]
+aVoletCharles.ActionsCommands ={7:"self.currentStatus=\"ouvert\"", 8:"self.currentStatus=\"ferme\""}
+
+aLampeSecondaireCharles = Object()
+aLampeSecondaireCharles.physicalLocation = "Charles"
+aLampeSecondaireCharles.id = 3
+aLampeSecondaireCharles.description = "Lampe secondaire charles"
+aLampeSecondaireCharles.InPossibleCmd = {11:"on",12:"off"}
+aLampeSecondaireCharles.OutPossibleCmd = {}
+aLampeSecondaireCharles.PossibleStates = ["on","off"]
+aLampeSecondaireCharles.ActionsCommands ={11:"self.currentStatus=\"on\"", 12:"self.currentStatus=\"off\""}
+
 aDetecteurFumee = Object()
 aDetecteurFumee.physicalLocation = "Cuisine"
+aDetecteurFumee.id = 2
+aDetecteurFumee.description = "Detecteur fumee cuisine"
+aDetecteurFumee.Reset = "self.currentStatus=\"pas incendie\""
 aDetecteurFumee.InPossibleCmd = {39:"ping avec reponse",40:"Buz"}
-aDetecteurFumee.OutPossibleCmd = {444:"incendie ongoing"}
+aDetecteurFumee.OutPossibleCmd = {1:"incendie ongoing"}
 aDetecteurFumee.PossibleStates = ["incendie en cours","pas incendie"]
-aDetecteurFumee.ActionsCommands ={39:"", 40:"",444:"print(\"au feu\")"}
+aDetecteurFumee.ActionsCommands ={39:"", 40:"",1:"self.currentStatus=\"incendie en cours\""}
+
+aDetecteurEntree = Object()
+aDetecteurEntree.physicalLocation = "Entree"
+aDetecteurEntree.id = 6
+aDetecteurEntree.Reset = "self.currentStatus=\"pas personne detecte\""
+aDetecteurEntree.description = "Detecteur entree"
+aDetecteurEntree.InPossibleCmd = {34:"allume lumiere",35:"teindre lumiere",36:"allume lumiere 2 min",33:"debug simule passage detecteur"}
+aDetecteurEntree.OutPossibleCmd = {50:"personne detecte"}
+aDetecteurEntree.PossibleStates = ["personne detecte","pas personne detecte"]
+aDetecteurEntree.ActionsCommands ={34:"", 35:"", 36:"", 33:"",50:"self.currentStatus=\"personne detecte\""}
+
+aDetecteurCharles = Object()
+aDetecteurCharles.physicalLocation = "Charles"
+aDetecteurCharles.id = 7
+aDetecteurCharles.Reset = "self.currentStatus=\"pas personne detecte\""
+aDetecteurCharles.description = "Detecteur entree"
+aDetecteurCharles.InPossibleCmd = {34:"allume lumiere",35:"teindre lumiere",36:"allume lumiere 2 min",33:"debug simule passage detecteur"}
+aDetecteurCharles.OutPossibleCmd = {2:"personne detecte"}
+aDetecteurCharles.PossibleStates = ["personne detecte","pas personne detecte"]
+aDetecteurCharles.ActionsCommands ={34:"", 35:"", 36:"", 33:"",2:"self.currentStatus=\"personne detecte\""}
+
 
 aRegisterDevices =[]
 aRegisterDevices.append(aLampeCharles)
 aRegisterDevices.append(aDetecteurFumee)
+aRegisterDevices.append(aLampeSecondaireCharles)
+aRegisterDevices.append(aVoletCharles)
+aRegisterDevices.append(aDetecteurEntree)
+aRegisterDevices.append(aDetecteurCharles)
 
 host = ''
 port = int(options.aPortToUse)
@@ -178,31 +240,33 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host,port))
 server.listen(backlog)
 input = [server,fd]
-print ("Daemon starting")
 while 1:
     inputready,outputready,exceptready = select.select(input,[],[])
 
     for s in inputready:
 
         if s == server:
-            print ("handle the server socket")
+            logging.info("handle the server socket")
             client, address = server.accept()
             input.append(client)
             
         elif s == fd:
-            print ("handle USB port input")
+            logging.info("handle USB port input")
             aResponse = fd.readline()
-            HandleUsbInput(aResponse)
+            HandleUsbInput(aResponse,aRegisterDevices)
 
         else:
-            print ("handle all other sockets")
+            logging.info("handle all other sockets")
             data = s.recv(size)
-            print ("input is : " + data)
+            logging.info("input is : " + data)
             if data:
-                SendMessage(fd,data)
+                SendMessage(fd,data,aRegisterDevices)
                 s.send("ACK")
             else:
-                print ("Closing socket")
+                logging.info("Closing socket")
                 s.close()
                 input.remove(s)
+                
+    smartProcessing(aRegisterDevices,fd)
+                
 server.close() 
